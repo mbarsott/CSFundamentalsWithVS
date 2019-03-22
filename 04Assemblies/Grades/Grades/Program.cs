ba95cdf750e84d7d9b8e1ea1e3f7982f4868c13d
Marcelo Barsotti <mbarsott@gmail.com>
2019-03-22 17:26:17 -0400

    4. Assemblies

---
+++ 04Assemblies/Grades/Grades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+//            SpeechSynthesizer synth = new SpeechSynthesizer();
+//            synth.Speak("The average grade is " + stats.AverageGrade);
+//            synth.Speak("The highest grade is " + stats.HighestGrade);
+//            synth.Speak("The lowest grade is " + stats.LowestGrade);
+        }
+    }
+}
