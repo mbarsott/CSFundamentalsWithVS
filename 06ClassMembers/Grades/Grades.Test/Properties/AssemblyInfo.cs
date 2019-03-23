ed1fa8af460a1dbc3baebc2224cb58b821f68048
Marcelo Barsotti <mbarsott@gmail.com>
2019-03-23 19:44:00 -0400

    6. Methods, Fields, Events, and Properties

---
+++ 06ClassMembers/Grades/Grades.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grades.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grades.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("43244fa6-371c-4c3f-b4a4-e8fca55825ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 06ClassMembers/Grades/Grades/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Grades
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var book = new GradeBook();
+//            book.NameChanged += new NameChangedDelegate(OnNameChanged);
+//            book.NameChanged += new NameChangedDelegate(OnNameChanged2);
+//            book.NameChanged += null;
+            book.NameChanged += OnNameChanged;
+//            book.NameChanged += OnNameChanged2;
+//            book.NameChanged += OnNameChanged2;
+//            book.NameChanged -= OnNameChanged2;
+            book.Name = "Marcelo's Grade Book";
+            book.Name = null;
+            book.Name = "Grade Book";
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            var stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int) stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+        }
+
+//        static void OnNameChanged2(string from, string to)
+//        {
+//            Console.WriteLine("***");
+//        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+
+        private static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + " " + result);
+        }
+
+        private static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}"); // C#6 and later
+//            Console.WriteLine("{0}: {1:F2}", description, result); // F2 float with 2 decimals; C for currency, etc.
+        }
+    }
+}
