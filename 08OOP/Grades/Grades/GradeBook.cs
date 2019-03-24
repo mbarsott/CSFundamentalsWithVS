743b8169cb4a2f8927864cf07b9fce535962da2e
Marcelo Barsotti <mbarsott@gmail.com>
2019-03-24 16:03:07 -0400

    8.5 Object Oriented Programming - Abstract Classes

---
+++ 08OOP/Grades/Grades/GradeBook.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Grades
+{
+//    public class GradeBook : object // inheriting from object is the default inheritance for a class
+    public class GradeBook : GradeTracker
+    {
+        public GradeBook()
+        {
+            _name = "Empty";
+            grades = new List<float>();
+        }
+
+        public override GradeStatistics ComputeStatistics()
+        {
+            Console.WriteLine("GradeBook::ComputeStatistics");
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
+        protected List<float> grades;
+
+        public override void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public override void WriteGrades(TextWriter destination)
+        {
+//            for (int i = 0; i < grades.Count; i++)
+//            {
+//                destination.WriteLine(grades[i]);
+//            }
+            for (int i = grades.Count; i > 0; i--)
+            {
+                destination.WriteLine(grades[i - 1]);
+            }
+        }
+    }
+}
---
+++ 08OOP/Grades/Grades/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace Grades
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GradeTracker book = CreateGradeBook();
+
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
+
+        private static GradeTracker CreateGradeBook()
+        {
+            return new ThrowAwayGradeBook();
+        }
+
+        private static void WriteResults(GradeTracker book)
+        {
+            var stats = book.ComputeStatistics();
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+            WriteResult("Grade", stats.LetterGrade + " - " + stats.Description);
+        }
+
+        private static void SaveGrades(GradeTracker book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void AddGrades(GradeTracker book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(GradeTracker book)
+        {
+            try
+            {
+                Console.WriteLine("Enter a name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //            catch (NullReferenceException ex)
+            //            {
+            //                Console.WriteLine("Something went wrong!");
+            //            }
+        }
+
+        private static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result}");
+        }
+
+        private static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2}");
+        }
+    }
+}
